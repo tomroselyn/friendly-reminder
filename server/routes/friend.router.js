f4041a151fce85e1d6c2bae7c76b5bef3475a5ae
Thomas Roselyn <tgroselyn@gmail.com>
2019-05-30 13:26:32 -0500

    update route working

--- server/routes/friend.router.js
+++ server/routes/friend.router.js
@@ -2,20 +2,33 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//DELETE friend by id
+router.delete('/:id', (req, res) => {
+    const deleteQuery = `DELETE FROM "friend" WHERE "id" = $1 AND "user_id" = $2;`;
+    pool.query(deleteQuery, [req.params.id, req.user.id])
+        .then(deleteQueryResult => {
+            res.sendStatus(204);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        }); //end deleteQuery
+
+}); //end DELETE friend by id
+
 //GET friend
 router.get('/', (req, res) => {
-    const friendQuery = `SELECT "friend".id, "friend".first_name, "friend".last_name, "address".email, "address".sms, "address".url, "address".pref, "timing".frequency, "timing".last_type, "timing".last_date, "timing".due_date FROM "friend"
+    const getQuery = `SELECT "friend".id, "friend".first_name, "friend".last_name, "address".email, "address".sms, "address".url, "address".pref, "timing".frequency, "timing".last_type, "timing".last_date, "timing".due_date FROM "friend"
     JOIN "address" ON "address".friend_id = "friend".id
     JOIN "timing" ON "timing".friend_id = "friend".id
     WHERE "user_id" = $1;`;
-    pool.query(friendQuery, [req.user.id])
-    .then(friendQueryResult => {
-        res.send(friendQueryResult.rows);
+    pool.query(getQuery, [req.user.id])
+    .then(getQueryResult => {
+        res.send(getQueryResult.rows);
     }).catch(err => {
         console.log(err);
         res.sendStatus(500);
-    }) //end friendQuery
-    
+    }); //end getQuery
+
 }); //end GET friend
 
 //POST friend
@@ -51,8 +64,43 @@ router.post('/', (req, res) => {
     }).catch(err => {
         console.log(err);
         res.sendStatus(500);
-    }) //end friend query
+    }); //end friend query
 
 }); //end POST friend
+
+//PUT friend by id (update everything)
+router.put('/:id', (req, res) => {
+    const friend = req.body;
+    const dueDate = '2020/01/09';
+
+    const updateFriendQuery = `UPDATE "friend" SET "first_name" = $1, "last_name" = $2 WHERE "id" = $3 AND "user_id" = $4;`;
+    pool.query(updateFriendQuery, [friend.first_name, friend.last_name, req.params.id, req.user.id])
+        .then(updateFriendQueryResult => {
+
+            const updateAddressQuery = `UPDATE "address" SET "email" = $1, "sms" = $2, "url" = $3 WHERE "friend_id" = $4;`;
+            pool.query(updateAddressQuery, [friend.email, friend.sms, friend.url, req.params.id])
+                .then(updateAddressQueryResult => {
+
+                    const updateTimingQuery = `UPDATE "timing" SET "frequency" = $1, "last_type" = $2, "last_date" = $3, "due_date" = $4 WHERE "friend_id" = $5;`;
+                    pool.query(updateTimingQuery, [friend.frequency, friend.last_type, friend.last_date, dueDate, req.params.id])
+                    .then(timingQueryResult => {
+                        res.sendStatus(200);
+
+                    }).catch(err => {
+                        console.log(err);
+                        res.sendStatus(500);
+                    }) //end update timing
+
+                }).catch(err => {
+                    console.log(err);
+                    res.sendStatus(500);
+                }) //end update address
+
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        }); //end update friend
+
+}); //end PUT friend by id (update everything)
 
 module.exports = router;
