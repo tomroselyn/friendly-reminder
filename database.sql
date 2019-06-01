7fc4df6aa60b31b6ded33f1eb51d720608d20024
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-01 12:55:16 -0500

    route auth and database setup instructions

--- database.sql
+++ database.sql
@@ -3,8 +3,50 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- database setup instructions --
+
+--create user table --> collecting first name and last name on registration
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (50) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR (80) NOT NULL,
+    "last_name" VARCHAR (80) NOT NULL
 );
+
+--create ENUM for contact address types (email, SMS, URL)
+CREATE TYPE ADDRESS_TYPE AS ENUM ('email', 'sms', 'url');
+
+--create friend table --> holds each friend with a reference to the address book owner
+CREATE TABLE "friend" (
+	"id" SERIAL PRIMARY KEY,
+	"first_name" VARCHAR (80) NOT NULL,
+	"last_name" VARCHAR (80) NOT NULL,
+	"user_id" INT NOT NULL REFERENCES "user" ON DELETE CASCADE
+);
+
+--create address table --> holds addresses for each contact
+CREATE TABLE "address" (
+	"id" SERIAL PRIMARY KEY,
+	"email" VARCHAR(50) UNIQUE,
+	"sms" VARCHAR(10) UNIQUE,
+	"url" VARCHAR(255),
+	"pref" ADDRESS_TYPE NOT NULL,
+	"friend_id" INT UNIQUE NOT NULL REFERENCES "friend" ON DELETE CASCADE
+	CONSTRAINT address_1 CHECK (NOT pref = 'email'::address_type OR email::text !~~ ''::text),
+	CONSTRAINT address_2 CHECK (NOT pref = 'sms'::address_type OR sms::text !~~ ''::text),
+	CONSTRAINT address_3 CHECK (NOT pref = 'url'::address_type OR url::text !~~ ''::text)
+);
+
+--create timing table --> holds contact frequency and date info
+CREATE TABLE "timing" (
+	"id" SERIAL PRIMARY KEY,
+	"frequency" INT NOT NULL,
+	"last_type" ADDRESS_TYPE,
+	"last_date" DATE NOT NULL,
+	"due_date" DATE NOT NULL,
+	"friend_id" INT UNIQUE NOT NULL REFERENCES "friend" ON DELETE CASCADE
+);
+
+-- end database setup instructions --
--- server/routes/friend.router.js
+++ server/routes/friend.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 //DELETE friend by id
-router.delete('/:id', (req, res) => {
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
     const deleteQuery = `DELETE FROM "friend" WHERE "id" = $1 AND "user_id" = $2;`;
     pool.query(deleteQuery, [req.params.id, req.user.id])
         .then(deleteQueryResult => {
@@ -17,7 +17,7 @@ router.delete('/:id', (req, res) => {
 }); //end DELETE friend by id
 
 //GET friend
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     const getQuery = `SELECT "friend".id, "friend".first_name, "friend".last_name, "address".email, "address".sms, "address".url, "address".pref, "timing".frequency, "timing".last_type, "timing".last_date, "timing".due_date FROM "friend"
     JOIN "address" ON "address".friend_id = "friend".id
     JOIN "timing" ON "timing".friend_id = "friend".id
@@ -54,18 +54,18 @@ router.post('/', rejectUnauthenticated, async (req, res) => {
         const newFriendId = result.rows[0].id;
 
         //then, add address
-        const addressQuery = `INSERT INTO "address" ("email", "sms", "url", "pref", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
-        const addressValues = [friend.email, friend.sms, friend.url, friend.pref, newFriendId];
-        await connection.query(addressQuery, addressValues);
+        const addAddressQuery = `INSERT INTO "address" ("email", "sms", "url", "pref", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
+        const addAddressValues = [friend.email, friend.sms, friend.url, friend.pref, newFriendId];
+        await connection.query(addAddressQuery, addAddressValues);
 
         //then, add timing
-        const timingQuery = `INSERT INTO "timing" ("frequency", "last_type", "last_date", "due_date", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
-        const timingValues = [friend.frequency, friend.last_type, friend.last_date, dueDate, newFriendId];
-        await connection.query(timingQuery, timingValues);
+        const addTimingQuery = `INSERT INTO "timing" ("frequency", "last_type", "last_date", "due_date", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
+        const addTimingValues = [friend.frequency, friend.last_type, friend.last_date, dueDate, newFriendId];
+        await connection.query(addTimingQuery, addTimingValues);
 
         //commit the transaction
         await connection.query('COMMIT');
-            res.sendStatus(200);
+            res.sendStatus(201);
 
     } catch (error) {
         //if any steps fail, abort entire transaction
@@ -78,83 +78,52 @@ router.post('/', rejectUnauthenticated, async (req, res) => {
     }
 }); //end POST
 
-//POST friend
-// router.post('/', (req, res) => {
-//     const friend = req.body
-//     const dueDate = new Date(friend.last_date);
-//     dueDate.setDate(dueDate.getDate() + (friend.frequency * 7));
-    
-//     // `${friend.last_date} + integer '${friend.frequency * 7}'`;
+//PUT everything using SQL transaction
+router.put('/:id', rejectUnauthenticated, async (req, res) => {
+    const friend = req.body
+    //calculate due date
+    const dueDate = new Date(friend.last_date);
+    dueDate.setDate(dueDate.getDate() + (friend.frequency * 7));
 
-//     //friend query
-//     const friendQuery = `INSERT INTO "friend" ("first_name", "last_name", "user_id") VALUES ($1, $2, $3) RETURNING "id";`;
-//     pool.query(friendQuery, [friend.first_name, friend.last_name, req.user.id])
-//     .then(friendQueryResult => {
+    //use the same connection for all queries
+    const connection = await pool.connect()
 
-//         //address query
-//         const addressQuery = `INSERT INTO "address" ("email", "sms", "url", "pref", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
-//         pool.query(addressQuery, [friend.email, friend.sms, friend.url, friend.pref, friendQueryResult.rows[0].id])
-//         .then(addressQueryResult => {
+    try {
+        //begin the transaction
+        await connection.query('BEGIN');
 
-//             //timing query
-//             const timingQuery = `INSERT INTO "timing" ("frequency", "last_type", "last_date", "due_date", "friend_id") VALUES ($1, $2, $3, $4, $5);`;
-//             pool.query(timingQuery, [friend.frequency, friend.last_type, friend.last_date, dueDate, friendQueryResult.rows[0].id])
-//             .then(timingQueryResult => {
-//                 res.sendStatus(200);
-//             }).catch(err => {
-//                 console.log(err);
-//                 res.sendStatus(500);
-//             }) //end timing query
-        
-//         }).catch(err => {
-//             console.log(err);
-//             res.sendStatus(500);
-//         }) //end address query
+        //update friend first
+        const updateFriendQuery = `UPDATE "friend" SET "first_name" = $1, "last_name" = $2 WHERE "id" = $3 AND "user_id" = $4;`;
+        const updateFriendValues = [friend.first_name, friend.last_name, req.params.id, req.user.id];
+        await connection.query(updateFriendQuery, updateFriendValues);
 
-//     }).catch(err => {
-//         console.log(err);
-//         res.sendStatus(500);
-//     }); //end friend query
+        //then, update address
+        const updateAddressQuery = `UPDATE "address" SET "email" = $1, "sms" = $2, "url" = $3, "pref" = $4 WHERE "friend_id" = $5;`;
+        const updateAddressValues = [friend.email, friend.sms, friend.url, friend.pref, req.params.id];
+        await connection.query(updateAddressQuery, updateAddressValues);
 
-// }); //end POST friend
+        //then, add timing
+        const updateTimingQuery = `UPDATE "timing" SET "frequency" = $1, "last_type" = $2, "last_date" = $3, "due_date" = $4 WHERE "friend_id" = $5;`;
+        const updateTimingValues = [friend.frequency, friend.last_type, friend.last_date, dueDate, req.params.id];
+        await connection.query(updateTimingQuery, updateTimingValues);
 
-//PUT friend by id (update everything)
-router.put('/:id', (req, res) => {
-    const friend = req.body;
-    const dueDate = '2020/01/09';
+        //commit the transaction
+        await connection.query('COMMIT');
+        res.sendStatus(200);
 
-    const updateFriendQuery = `UPDATE "friend" SET "first_name" = $1, "last_name" = $2 WHERE "id" = $3 AND "user_id" = $4;`;
-    pool.query(updateFriendQuery, [friend.first_name, friend.last_name, req.params.id, req.user.id])
-        .then(updateFriendQueryResult => {
+    } catch (error) {
+        //if any steps fail, abort entire transaction
+        await connection.query('ROLLBACK');
+        console.log('transaction error - rolling back update friend:', error);
+        res.sendStatus(500);
 
-            const updateAddressQuery = `UPDATE "address" SET "email" = $1, "sms" = $2, "url" = $3 WHERE "friend_id" = $4;`;
-            pool.query(updateAddressQuery, [friend.email, friend.sms, friend.url, req.params.id])
-                .then(updateAddressQueryResult => {
-
-                    const updateTimingQuery = `UPDATE "timing" SET "frequency" = $1, "last_type" = $2, "last_date" = $3, "due_date" = $4 WHERE "friend_id" = $5;`;
-                    pool.query(updateTimingQuery, [friend.frequency, friend.last_type, friend.last_date, dueDate, req.params.id])
-                    .then(timingQueryResult => {
-                        res.sendStatus(200);
-
-                    }).catch(err => {
-                        console.log(err);
-                        res.sendStatus(500);
-                    }) //end update timing
-
-                }).catch(err => {
-                    console.log(err);
-                    res.sendStatus(500);
-                }) //end update address
-
-        }).catch(err => {
-            console.log(err);
-            res.sendStatus(500);
-        }); //end update friend
-
-}); //end PUT friend by id (update everything)
+    } finally {
+        connection.release()
+    }
+}); //end PUT everything
 
 //PUT extra day by id
-router.put('/extraday/:id', (req, res) => {
+router.put('/extraday/:id', rejectUnauthenticated, (req, res) => {
     const extraDayQuery = `UPDATE "timing" SET "due_date" = "due_date" + integer '1' WHERE "friend_id" = $1;`;
     pool.query(extraDayQuery, [req.params.id])
     .then(result => {
