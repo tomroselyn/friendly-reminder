98f4766286d5d70f5a75284323e381c91634d88c
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-03 13:42:27 -0500

    added nodemailer and set up email router

---
+++ server/routes/email.router.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
+
+//email transporter
+const nodemailer = require('nodemailer');
+
+const transport = {
+    host: 'smtp.gmail.com',
+    auth: {
+        user: process.env.email_user,
+        pass: process.env.email_pass
+    }
+}
+
+const transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Server is ready to take messages');
+    }
+});
+
+//POST route to send email
+router.post('/send', (req, res) => {
+    const name = `${req.user.first_name} ${req.user.last_name}`;
+    const sendEmail = req.user.username;
+    const message = req.body.message
+    const content = `from: ${name} \n reply to: ${sendEmail} \n message: ${message} `
+
+    const mail = {
+        from: name,
+        to: req.body.recEmail,  //Change to email address that you want to receive messages on
+        subject: req.body.subject,
+        text: content
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                msg: 'fail'
+            })
+        } else {
+            res.json({
+                msg: 'success'
+            })
+        }
+    })
+}); //end POST route
+
+module.exports = router;
